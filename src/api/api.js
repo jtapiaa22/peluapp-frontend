b57b86294de140d9184190c0ceed1a8b8b25acd0
Jorge Tapia Ahumada <jorgealejandrotapiaahumada@gmail.com>
2026-02-26 20:53:40 -0300

    abse

--- src/api/api.js
+++ src/api/api.js
@@ -1,24 +1,20 @@
 import axios from 'axios'
 
-const apiLocal = axios.create({
-  baseURL: 'http://localhost:3001/api'
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL
 })
 
-const apiLicencias = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3001/api'
-})
+export const getStats         = ()          => api.get('/stats')
+export const getClientes      = ()          => api.get('/clientes')
+export const getCliente       = (id)        => api.get(`/clientes/${id}`)
+export const createCliente    = (data)      => api.post('/clientes', data)
+export const updateCliente    = (id, data)  => api.put(`/clientes/${id}`, data)
+export const deleteCliente    = (id)        => api.delete(`/clientes/${id}`)
+export const getLicencias     = (clienteId) => api.get(`/licencias/cliente/${clienteId}`)
+export const createLicencia   = (data)      => api.post('/licencias', data)
+export const deleteLicencia   = (id)        => api.delete(`/licencias/${id}`)
 
-export const getStats         = ()          => apiLocal.get('/stats')
-export const getClientes      = ()          => apiLocal.get('/clientes')
-export const getCliente       = (id)        => apiLocal.get(`/clientes/${id}`)
-export const createCliente    = (data)      => apiLocal.post('/clientes', data)
-export const updateCliente    = (id, data)  => apiLocal.put(`/clientes/${id}`, data)
-export const deleteCliente    = (id)        => apiLocal.delete(`/clientes/${id}`)
-export const getLicencias     = (clienteId) => apiLocal.get(`/licencias/cliente/${clienteId}`)
-export const createLicencia   = (data)      => apiLocal.post('/licencias', data)
-export const deleteLicencia   = (id)        => apiLocal.delete(`/licencias/${id}`)
-
-export const enviarEmail = (licencia) => apiLicencias.post('/notificaciones/email', {
+export const enviarEmail = (licencia) => api.post('/notificaciones/email', {
   id:           licencia.id,
   nombre:       licencia.nombre,
   peluqueria:   licencia.peluqueria,
@@ -28,4 +24,4 @@ export const enviarEmail = (licencia) => apiLicencias.post('/notificaciones/emai
   licencia_b64: licencia.licencia_b64
 })
 
-export const getLicenciaDownloadUrl = (id) => `${apiLicencias.defaults.baseURL}/licencias/${id}/download`
+export const getLicenciaDownloadUrl = (id) => `${api.defaults.baseURL}/licencias/${id}/download`
